5b90eb42c611d33bd2fd20d7863d5e2ab78ffb20
Felipev98 <felipe_v_spd@hotmail.com>
2022-03-27 20:42:46 -0400

    add: vue dependecies and project config

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="nav">
+<h1>dojaodj</h1>
+  </div>
+  <router-view/>
+</template>
+
+<style lang="scss">
+@import 'assets/styles.scss';
+body{
+background-color: $primary_color;
+}
+</style>>
+
