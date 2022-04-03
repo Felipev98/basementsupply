7345eaf87d9305d687a9a41daba72dbddde1bd80
Felipev98 <felipe_v_spd@hotmail.com>
2022-04-03 17:16:47 -0400

    add: finish grid system config

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,12 @@
 
 <style lang="scss">
 @import './assets/styles/settings/_colors.settings.scss';
+@import './assets/styles/generics/_reset.generics.scss';
 body{
   background-color: $backgroundColor;
+}
+h2{
+  color: $primaryColor;
 }
 
 </style>>
